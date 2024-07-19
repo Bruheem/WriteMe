f6e18d6aa632c47f3ac6ddf7ae071e673edb64ef
Ben. Ibrahim <brqhim@pm.me>
2024-07-19 14:42:00 +0100

    Build API server skeleton
    - flags declaration
    - INFO logger declaration
    - added home handler
    - registered the home handler to the root URL
    
    - Used httprouter package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+// Server configuration struct
+type config struct {
+	port int
+	env  string
+}
+
+// Application dependencies
+type application struct {
+	cfg    config
+	logger *log.Logger
+}
 
 func main() {
-	fmt.Println("Hello there!")
+	// Define flags
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4000, "server network port")
+	flag.StringVar(&cfg.env, "environment", "development", "server environment")
+	flag.Parse()
+
+	// Define loggers
+	logger := log.New(os.Stdout, "INFO: ", log.Ltime)
+
+	app := &application{
+		cfg:    cfg,
+		logger: logger,
+	}
+
+	srv := http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.port),
+		Handler: app.routes(),
+	}
+
+	// Starting Server
+	logger.Printf("starting server on port %s", srv.Addr)
+	logger.Fatal(srv.ListenAndServe())
 }
