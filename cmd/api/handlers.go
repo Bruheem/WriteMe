04c80a76f97aeb394c1cdf470d5824cecd776085
Ben. Ibrahim <brqhim@pm.me>
2024-07-20 13:12:06 +0100

    Add all handlers && registering the routes

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -5,11 +5,17 @@ import (
 	"net/http"
 )
 
-func (app *application) home(w http.ResponseWriter, r *http.Request) {
+// Page handlers
+func (app *application) homeHandler(w http.ResponseWriter, r *http.Request) {
 	// The home handler is aimed to display the 4 most recent books modified or created
 	fmt.Fprintf(w, "Hello this is the home page!")
 }
 
+func (app *application) libraryHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Displaying your library...")
+}
+
+// Book handlers
 func (app *application) getBookHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readID(r)
 	if err != nil {
@@ -18,4 +24,37 @@ func (app *application) getBookHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Dispalying the %d book", id)
+}
+
+func (app *application) createBookHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "creating your book...")
+}
+
+func (app *application) updateBookHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "updating your book...")
+}
+
+func (app *application) deleteBookHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "deleting your book...")
+}
+
+// User Authentication handlers
+func (app *application) userSignupForm(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Displaying signing up form")
+}
+
+func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Signing a user...")
+}
+
+func (app *application) userLoginForm(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Displaying login form")
+}
+
+func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Logging in the user")
+}
+
+func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Logging out the user")
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,8 +10,19 @@ func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	// Handler registration to URLs
-	router.HandlerFunc(http.MethodGet, "/", app.home)
-	router.HandlerFunc(http.MethodGet, "/books/:id", app.getBookHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/", app.homeHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/library", app.libraryHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/books/:id", app.getBookHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/books/create", app.createBookHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/books/:id", app.updateBookHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/books/:id", app.deleteBookHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/user/signup", app.userSignupForm)
+	router.HandlerFunc(http.MethodPost, "/v1/user/signup", app.userSignupPost)
+	router.HandlerFunc(http.MethodGet, "/v1/user/login", app.userLoginForm)
+	router.HandlerFunc(http.MethodPost, "/v1/user/login", app.userLoginPost)
+	router.HandlerFunc(http.MethodPost, "/v1/user/logout", app.userLogoutPost)
 
 	return router
 }
